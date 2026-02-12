6b96a97c55931be5c9bf451a515769b370ccaf40
Gino Jang <devilnis@naver.com>
2026-02-12 13:15:00 +0900

    [feat] Sample Scene 정리 2차

---
+++ Assets/Scenes/_MMOCore/BaseSceneEnvrionment.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BaseSceneEnvrionment : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MyCharacterCore.Instance.CreateMyCharacter(DataManager.eCHARACTER_TYPE.BOY_04);
+        //InitPlayer(Vector3.zero, 0.0f);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
